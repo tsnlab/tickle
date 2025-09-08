6e217fdb53351ea4f54b0a711a76cd613b3b0ae3
Jeong Arm <arm@tsnlab.com>
2025-09-08 11:45:56 +0900

    Prepare some rmw implementations

---
+++ rmw_tickle/src/rmw_tickle_c/rmw_event.c
@@ -0,0 +1,104 @@
+// Copyright 2024 TickLE RMW Implementation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rmw_tickle_c/rmw_tickle.h"
+#include "rmw/rmw.h"
+#include "rcutils/error_handling.h"
+
+#include <stdio.h>
+
+rmw_ret_t
+rmw_event_set_callback(
+  rmw_event_t * event,
+  rmw_event_callback_t callback,
+  const void * user_data)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(event, RMW_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(callback, RMW_RET_INVALID_ARGUMENT);
+
+  if (strcmp(event->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION;
+  }
+
+  // For now, we don't support event callbacks in TickLE
+  // This is a placeholder implementation
+  (void)user_data;
+  
+  printf("rmw_event_set_callback: function not implemented for TickLE\n");
+  return RMW_RET_UNSUPPORTED;
+}
+
+rmw_ret_t
+rmw_subscription_set_on_new_message_callback(
+  rmw_subscription_t * subscription,
+  rmw_event_callback_t callback,
+  const void * user_data)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(subscription, RMW_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(callback, RMW_RET_INVALID_ARGUMENT);
+
+  if (strcmp(subscription->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION;
+  }
+
+  // For now, we don't support subscription callbacks in TickLE
+  (void)user_data;
+  
+  printf("rmw_subscription_set_on_new_message_callback: function not implemented for TickLE\n");
+  return RMW_RET_UNSUPPORTED;
+}
+
+rmw_ret_t
+rmw_service_set_on_new_request_callback(
+  rmw_service_t * service,
+  rmw_event_callback_t callback,
+  const void * user_data)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(service, RMW_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(callback, RMW_RET_INVALID_ARGUMENT);
+
+  if (strcmp(service->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION;
+  }
+
+  // For now, we don't support service callbacks in TickLE
+  (void)user_data;
+  
+  printf("rmw_service_set_on_new_request_callback: function not implemented for TickLE\n");
+  return RMW_RET_UNSUPPORTED;
+}
+
+rmw_ret_t
+rmw_client_set_on_new_response_callback(
+  rmw_client_t * client,
+  rmw_event_callback_t callback,
+  const void * user_data)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(client, RMW_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(callback, RMW_RET_INVALID_ARGUMENT);
+
+  if (strcmp(client->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION;
+  }
+
+  // For now, we don't support client callbacks in TickLE
+  (void)user_data;
+  
+  printf("rmw_client_set_on_new_response_callback: function not implemented for TickLE\n");
+  return RMW_RET_UNSUPPORTED;
+}
---
+++ rmw_tickle/src/rmw_tickle_c/rmw_node.c
@@ -0,0 +1,118 @@
+// Copyright 2024 TickLE RMW Implementation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rmw_tickle_c/rmw_tickle.h"
+#include "rmw/rmw.h"
+#include "rcutils/error_handling.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+rmw_node_t *
+rmw_create_node(
+  rmw_context_t * context,
+  const char * name,
+  const char * namespace_)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(context, NULL);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(name, NULL);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(namespace_, NULL);
+
+  if (strcmp(context->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return NULL;
+  }
+
+  if (strlen(name) == 0) {
+    RMW_SET_ERROR_MSG("Node name cannot be empty");
+    return NULL;
+  }
+
+  if (strlen(namespace_) == 0) {
+    RMW_SET_ERROR_MSG("Node namespace cannot be empty");
+    return NULL;
+  }
+
+  // Allocate memory for the node
+  rmw_tickle_node_t * tickle_node = malloc(sizeof(rmw_tickle_node_t));
+  if (tickle_node == NULL) {
+    RMW_SET_ERROR_MSG("Failed to allocate memory for node");
+    return NULL;
+  }
+
+  // Initialize the TickLE node structure
+  memset(tickle_node, 0, sizeof(rmw_tickle_node_t));
+  
+  // Set up the RMW node structure
+  rmw_node_t * rmw_node = malloc(sizeof(rmw_node_t));
+  if (rmw_node == NULL) {
+    free(tickle_node);
+    RMW_SET_ERROR_MSG("Failed to allocate memory for RMW node");
+    return NULL;
+  }
+
+  rmw_node->implementation_identifier = RMW_TICKLE_IDENTIFIER;
+  rmw_node->data = tickle_node;
+  rmw_node->name = name;
+  rmw_node->namespace_ = namespace_;
+
+  // Initialize TickLE node (placeholder - actual implementation would go here)
+  // tt_Node_init(&tickle_node->tickle_node, ...);
+
+  printf("Created TickLE node: %s%s\n", namespace_, name);
+
+  return rmw_node;
+}
+
+rmw_ret_t
+rmw_destroy_node(rmw_node_t * node)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(node, RMW_RET_INVALID_ARGUMENT);
+
+  if (strcmp(node->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION;
+  }
+
+  rmw_tickle_node_t * tickle_node = (rmw_tickle_node_t *)node->data;
+  if (tickle_node != NULL) {
+    // Cleanup TickLE node (placeholder - actual implementation would go here)
+    // tt_Node_fini(&tickle_node->tickle_node);
+    free(tickle_node);
+  }
+
+  free(node);
+
+  printf("Destroyed TickLE node: %s%s\n", node->namespace_, node->name);
+
+  return RMW_RET_OK;
+}
+
+const rmw_guard_condition_t *
+rmw_node_get_graph_guard_condition(
+  const rmw_node_t * node)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(node, NULL);
+
+  if (strcmp(node->implementation_identifier, RMW_TICKLE_IDENTIFIER) != 0) {
+    RMW_SET_ERROR_MSG("Implementation identifiers does not match");
+    return NULL;
+  }
+
+  // For now, we don't support graph guard conditions in TickLE
+  // This is a placeholder implementation that returns NULL
+  printf("rmw_node_get_graph_guard_condition: function not implemented for TickLE\n");
+  return NULL;
+}
