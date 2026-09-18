8675ea96e0d1ff79ce37b36392aaca0501e86a40
github-action-benchmark <github@users.noreply.github.com>
2026-09-18 20:29:14 +0900

    add rmw_tickle latency (buildfarm_perf_tests) (customSmallerIsBetter) benchmark result for 79fb8ee1d387fc47d716da68b76ae2f9d1b1930f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789730060337,
+  "lastUpdate": 1789730954507,
   "repoUrl": "https://github.com/tsnlab/tickle",
   "entries": {
     "Latency (ping/pong)": [
@@ -18442,6 +18442,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "rmw_tickle Struct16 sync latency",
             "value": 0.04522571428571428,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "distinct": true,
+          "id": "79fb8ee1d387fc47d716da68b76ae2f9d1b1930f",
+          "message": "Revert \"Add -H (periodic Heartbeat) to perf_client and a reliable+heartbeat loss-injection scenario\"\n\nThis reverts commit 6786dbfdd3c5eb42c477f730f379302d09d773d7.",
+          "timestamp": "2026-09-18T20:27:53+09:00",
+          "tree_id": "1f4e3c968502f8162b2e755410d0253f8a1f34fe",
+          "url": "https://github.com/tsnlab/tickle/commit/79fb8ee1d387fc47d716da68b76ae2f9d1b1930f"
+        },
+        "date": 1789730952085,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "rmw_tickle Array1k async latency",
+            "value": 0.1107242857142857,
+            "unit": "ms"
+          },
+          {
+            "name": "rmw_tickle Array1k sync latency",
+            "value": 0.04566142857142857,
+            "unit": "ms"
+          },
+          {
+            "name": "rmw_tickle Struct16 async latency",
+            "value": 0.05105857142857143,
+            "unit": "ms"
+          },
+          {
+            "name": "rmw_tickle Struct16 sync latency",
+            "value": 0.04951571428571428,
             "unit": "ms"
           }
         ]
